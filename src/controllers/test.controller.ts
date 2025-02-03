ff6c4f6c86ed89545c2bb3cb8bfbada18ed03d13
Vicente Espinosa <vnespinosa@uc.cl>
2025-02-03 20:40:53 -0300

    feat: add type for query and response

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,13 +1,22 @@
 const _ = require('lodash');
 const listService = require('../services/test.service');
+import { Request, Response } from 'express';
 
-async function list(req, res) {
-  const query = _.get(req, 'query', {});
+interface Query {
+  limit?: number;
+}
+
+interface ListServiceResponse {
+  page: any;
+}
+
+async function list(req: Request, res: Response): Promise<Response> {
+  const query: Query = _.get(req, 'query', {});
   try {
     const { limit } = query;
 
     console.log('limit', limit);
-    const { page } = await listService({ limit });
+    const { page }: ListServiceResponse = await listService({ limit });
 
     return res.status(200).json(page);
   } catch (error) {
