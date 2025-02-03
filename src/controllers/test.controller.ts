2d989cb666d2224eb08e5980ba10cebb8185af04
Vicente Espinosa <vnespinosa@uc.cl>
2025-02-03 20:38:37 -0300

    feat: add basic test service and controller

---
+++ src/controllers/test.controller.ts
@@ -0,0 +1,18 @@
+const _ = require('lodash');
+const listService = require('../services/test.service');
+
+async function list(req, res) {
+  const query = _.get(req, 'query', {});
+  try {
+    const { limit } = query;
+
+    console.log('limit', limit);
+    const { page } = await listService({ limit });
+
+    return res.status(200).json(page);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export default list;
---
+++ src/services/test.service.ts
@@ -0,0 +1,4 @@
+module.exports = async function list({ limit = 10 }) {
+
+  return { page: limit };
+};
