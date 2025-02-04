982f7daacf1be258f95fe347a432f4f607d41784
Vicente Espinosa <vnespinosa@uc.cl>
2025-02-03 21:20:02 -0300

    refactor: use database for list endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router, Request, Response } from "express";
 import testController from "../controllers/test.controller";
+import testcreateController from "../controllers/testCreate.controller";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   await testController(req, res);
+  return;
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  await testcreateController(req, res);
   return;
 });
 
--- src/services/test.service.ts
+++ src/services/test.service.ts
@@ -1,16 +1,6 @@
-const testItems = [
-  { id: 1, name: 'Test 1' },
-  { id: 2, name: 'Test 2' },
-  { id: 3, name: 'Test 3' },
-  { id: 4, name: 'Test 4' },
-  { id: 5, name: 'Test 5' },
-  { id: 6, name: 'Test 6' },
-  { id: 7, name: 'Test 7' },
-  { id: 8, name: 'Test 8' },
-  { id: 9, name: 'Test 9' },
-  { id: 10, name: 'Test 10' },
-];
+import TestItem from "../models/testItem.model"
 
-module.exports = async function list({ limit = 10 }) {
-  return {items: testItems.slice(0, limit) };
+module.exports = async function list() {
+  const testItems = await TestItem.find();
+  return { items: testItems };
 };
